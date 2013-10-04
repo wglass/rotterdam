f82b78c50c40ab82a4c836e328c8908675a2f9fc
William Glass <william.glass@gmail.com>
2013-10-03 17:13:19 -0700

    Flesh out setup.py more so installs work.
    * add a version
    * fill out install_requires so pypi picks up dependencies
    * make sure the lua files are included in the distribution

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from setuptools import setup
 
 setup(
-    name="Rotterdam",
+    name="rotterdam",
+    version="0.0.2",
     packages=["rotterdam"],
     scripts=[
         "bin/rotterdam",
         "bin/rotterdamctl"
     ],
+    install_requires=[
+        "cython==0.19.1",
+        "setproctitle",
+        "gevent>=1.0rc2",
+        "redis==2.7.6"
+    ],
+    dependency_links=[
+        "http://github.com/surfly/gevent.git/tarball/1.0rc2#egg=gevent-1.0rc2"
+    ],
     tests_require=[
         "mock==1.0.1",
         "nose==1.3.0"
-    ]
+    ],
+    package_data={
+        "rotterdam": ["lua/*.lua"]
+    }
 )
