5540c376a691be211f2ca2cb7db6f191157ce938
William Glass <william.glass@gmail.com>
2013-11-22 22:16:50 -0800

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="rotterdam",
-    version="0.1.2",
+    version="0.2.0",
     description=(
         "Simple distributed job queue via redis."
     ),
